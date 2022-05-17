9197e7c8417d99c5a64b5970359dba668563df33
Crespin <crespin.christophe@netc.eu>
2022-05-17 16:28:02 +0200

    First commit

---
+++ texte-service2/src/main/java/be/technifutur/Main.java
@@ -0,0 +1,16 @@
+package be.technifutur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+
+    }
+
+}
