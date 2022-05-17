63fda4eeae9af588945c070933e6aaf3f01795bc
Crespin <crespin.christophe@netc.eu>
2022-05-17 16:59:13 +0200

    Ajout du controller de print-service

---
+++ print-service/src/main/java/be/technifutur/controller/PrintServiceController.java
@@ -0,0 +1,30 @@
+package be.technifutur.controller;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/print")
+public class PrintServiceController {
+
+    private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
+
+    public PrintServiceController(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
+        this.discoveryClient = discoveryClient;
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping
+    public String getPrint(){
+
+        String responseBody = restTemplate.getForObject("http://texte-service/print", String.class);
+
+        return "Get to texte-service\n response ==> " + responseBody;
+
+    }
+
+}
