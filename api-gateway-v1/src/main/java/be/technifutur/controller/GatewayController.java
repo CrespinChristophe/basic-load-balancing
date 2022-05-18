d223714d825f51998dee2e08d6f46eb15a2ca081
Crespin <crespin.christophe@netc.eu>
2022-05-18 11:44:53 +0200

    Ajout de la classe GatewayController

---
+++ api-gateway-v1/src/main/java/be/technifutur/controller/GatewayController.java
@@ -0,0 +1,31 @@
+package be.technifutur.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GatewayController {
+
+    private final RestTemplate restTemplate;
+
+    public GatewayController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/print")
+    public String fromPrintService(){
+
+        return restTemplate.getForObject("http://print-service/print", String.class);
+
+    }
+
+    @GetMapping("/texte")
+    public String fromTexteService(){
+
+        return restTemplate.getForObject("htpp://texte-service/print", String.class);
+
+    }
+
+
+}
