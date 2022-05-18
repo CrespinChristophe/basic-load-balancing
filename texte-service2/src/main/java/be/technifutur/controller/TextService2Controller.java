0cb0db286bd053d31e2b32ca64a97147dc4b0d55
Crespin <crespin.christophe@netc.eu>
2022-05-18 10:29:45 +0200

    Ajout du controller de text-service2

--- texte-service2/src/main/java/be/technifutur/controller/TextService2Controller.java
+++ texte-service2/src/main/java/be/technifutur/controller/TextService2Controller.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/print")
-public class TextService1Controller {
+public class TextService2Controller {
 
     @GetMapping
     public String response(){
-        return "communication reussie avec text-service1";
+        return "communication reussie avec text-service2";
     }
 
 }
