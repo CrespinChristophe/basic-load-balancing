f5e3afed06ade1d7655a509c35ae16151e52647a
Crespin <crespin.christophe@netc.eu>
2022-05-19 14:48:36 +0200

    Ajout de la methode getMot au controller de print-service

--- print-service/src/main/java/be/technifutur/controller/PrintServiceController.java
+++ print-service/src/main/java/be/technifutur/controller/PrintServiceController.java
@@ -3,11 +3,12 @@ package be.technifutur.controller;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/print")
+//@RequestMapping("/print")
 public class PrintServiceController {
 
     private final DiscoveryClient discoveryClient;
@@ -18,13 +19,18 @@ public class PrintServiceController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping
+    @GetMapping("/print")
     public String getPrint(){
 
         String responseBody = restTemplate.getForObject("http://texte-service/texte", String.class);
 
         return "Get to texte-service\n response ==> " + responseBody;
 
+    }
+
+    @GetMapping("/mot")
+    public String getMot(@RequestParam String mot){
+        return "Votre mot est ==> " + mot;
     }
 
 }
